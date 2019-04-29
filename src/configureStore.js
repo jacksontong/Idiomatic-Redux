96031fc22c8ef3bf0e19bbebd8aea35f99203056
Jackson Tong <tongtoan2704@gmail.com>
2019-04-29 22:06:48 +0700

    12-Wrapping dispatch() to Log Actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,13 @@
 // @flow
 import { loadState, saveState } from "./localStorage";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux"
 import reducers from "./reducers";
 import throttle from "lodash/throttle";
+import logger from 'redux-logger'
 
 const configureStore = () => {
     const initialState = loadState()
-    const store = createStore(reducers, initialState)
+    const store = createStore(reducers, initialState, applyMiddleware(logger))
 
     store.subscribe(throttle(() => {
         saveState({
